ee7bed58834f5551a50968f78d02798723cb7eef
Deepin Developer <deepin-dev@deepin.org>
2022-09-14 15:25:10 +0800

    feat: Init commit
    init smplayer
    
    Log:

---
+++ create_deb.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+ 
+ln -s debian-orig debian
+cp debian-orig/changelog-orig debian-orig/changelog
+
+./get_svn_revision.sh
+#dch -v 18.10.0-`cat svn_revision` "New version"
+
+#dpkg-buildpackage -rfakeroot
+
+# This should be faster:
+rm build-stamp
+rm src/smplayer
+fakeroot debian/rules build
+fakeroot debian/rules binary
+
+dh_clean
+rm debian-orig/changelog
+rm debian
